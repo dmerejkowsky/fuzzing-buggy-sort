1561e50b3608634f104726b7882ccd9a24531567
Dimitri Merejkowsky <dimitri@dmerej.info>
2024-12-13 11:11:04 +0100

    Better starting point

--- buggy_sort.py
+++ buggy_sort.py
@@ -7,14 +7,14 @@ def larger_than(xs, pivot):
 
 
 def sortk(x, xs):
-    result = sort(smaller_than(xs, x))
-    last_part = sort(larger_than(xs, x))
+    result = buggy_sort(smaller_than(xs, x))
+    last_part = buggy_sort(larger_than(xs, x))
     result.append(x)
     result += last_part
     return result
 
 
-def sort(list):
+def buggy_sort(list):
     if not list:
         return []
     else:
